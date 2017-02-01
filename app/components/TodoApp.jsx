eae314523a8f67dfc447de0cd9a27099b7f25f52
nbento <ratinhodaprogramacao@gmail.com>
2017-02-01 09:57:58 +0000

    Lec 90 TodoSearch.jsx

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,12 +1,15 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
 	//............
 	getInitialState: function()
 	{
 		return {
+			showCompleted : false,  //  true | false
+			searchText: '', 		//string
 			todos: [
 				{
 					id: 1,
@@ -33,6 +36,15 @@ var TodoApp = React.createClass({
 	{
 		console.log('TodoApp  »»»  handleAddTodo text:::' + text);
 	},	
+	//............ TodoSearch callback		
+	handleSearch: function(showCompleted, searchText)
+	{
+		console.log('TodoApp  »»»  handleSearch searchText:::' + searchText );
+		this.setState({ 
+						showCompleted: 	showCompleted,
+						searchText: 	searchText.toLowerCase()
+					});
+	},	
 	//............		
 	render: function()
 	{
@@ -40,6 +52,7 @@ var TodoApp = React.createClass({
 
 		return (
 				<div>
+					<TodoSearch onSearch={this.handleSearch}/>
 					<TodoList todos={todos}/>
 					<AddTodo handleAddTodo={this.handleAddTodo} />
 				</div>
---
+++ app/components/TodoSearch.jsx
@@ -0,0 +1,44 @@
+var React = require('react');
+
+var TodoSearch = React.createClass({
+	//...........
+	/*getDefaultProps: function()
+	{
+		return ({  
+					onSearch: function(param1, param2) 
+		})
+	},*/
+	//........... onChange no inputtext e na checkbox
+	handleSearch: function()
+	{
+		var showCompleted = this.refs.showCompleted.checked; 	//checkbox
+		var searchText = this.refs.searchText.value; 			//inputtext
+
+		 this.props.onSearch(showCompleted, searchText);
+	},
+	//...........
+	render: function()
+	{
+		return (<div>
+					<div>
+						<input type="text" 
+								ref="searchText"
+								placeholder="Search todos"
+								onChange={this.handleSearch} />
+					</div>
+
+					<div>
+						<label>
+								<input type="checkbox" 
+										ref="showCompleted"
+										placeholder="Search todos"
+										onChange={this.handleSearch} />
+								Show Completed todos		
+						</label>
+					</div>
+			</div>)
+
+	}
+});
+
+module.exports = TodoSearch;		
